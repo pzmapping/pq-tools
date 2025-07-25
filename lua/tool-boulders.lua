2d0eded9c01fa8d0c85631f30e5f9a106ab7c84d
Unofficial PZ Mapping Discord <pzmapping@gmail.com>
2025-07-25 19:28:38 +0200

    Add files via upload

---
+++ lua/tool-boulders.lua
@@ -0,0 +1,644 @@
+--[[
+    Boulder Tool Created by Pabbiqo    
+]]
+
+
+
+
+DATA = {
+	
+    {
+        label = '## Medium Boulders ##',
+    },
+  
+    	{
+        label = 'Small 8',
+        dir1 = {
+            w = 'boulders_8'
+        },
+    },
+
+    	{
+        label = 'Small 9',
+        dir1 = {
+            w = 'boulders_9'
+        },
+    },
+
+    	{
+        label = 'Small 10',
+        dir1 = {
+            w = 'boulders_10'
+        },
+    },
+
+    	{
+        label = 'Small 11',
+        dir1 = {
+            w = 'boulders_11'
+        },
+    },
+
+    	{
+        label = 'Small 12',
+        dir1 = {
+            w = 'boulders_12'
+        },
+    },
+
+    	{
+        label = 'Small 13',
+        dir1 = {
+            w = 'boulders_13'
+        },
+    },
+
+    	{
+        label = 'Small 14',
+        dir1 = {
+            w = 'boulders_14'
+        },
+    },
+
+    	{
+        label = 'Small 15',
+        dir1 = {
+            w = 'boulders_15'
+        },
+    },
+
+    	{
+        label = 'Small 48',
+        dir1 = {
+            w = 'boulders_48'
+        },
+    },
+
+    	{
+        label = 'Small 49',
+        dir1 = {
+            w = 'boulders_49'
+        },
+    },
+
+    	{
+        label = 'Small 50',
+        dir1 = {
+            w = 'boulders_50'
+        },
+    },
+
+    	{
+        label = 'Small 51',
+        dir1 = {
+            w = 'boulders_51'
+        },
+    },
+
+    	{
+        label = 'Small 52',
+        dir1 = {
+            w = 'boulders_52'
+        },
+    },
+
+    	{
+        label = 'Small 53',
+        dir1 = {
+            w = 'boulders_53'
+        },
+    },
+
+    	{
+        label = 'Small 54',
+        dir1 = {
+            w = 'boulders_54'
+        },
+    },
+
+    	{
+        label = 'Small 55',
+        dir1 = {
+            w = 'boulders_55'
+        },
+    },
+
+
+    {
+        label = '## Medium Boulders ##',
+    },
+  
+    	{
+        label = 'Medium 0',
+        dir1 = {
+            w = 'boulders_0'
+        },
+    },
+
+	{
+        label = 'Medium 1',
+        dir1 = {
+            w = 'boulders_1'
+        },
+    },
+
+    	{
+        label = 'Medium 2',
+        dir1 = {
+            w = 'boulders_2'
+        },
+    },
+
+	{
+        label = 'Medium 3',
+        dir1 = {
+            w = 'boulders_3'
+        },
+    },
+
+    	{
+        label = 'Medium 4',
+        dir1 = {
+            w = 'boulders_4'
+        },
+    },
+
+	{
+        label = 'Medium 5',
+        dir1 = {
+            w = 'boulders_5'
+        },
+    },
+
+    	{
+        label = 'Medium 6',
+        dir1 = {
+            w = 'boulders_6'
+        },
+    },
+
+	{
+        label = 'Medium 7',
+        dir1 = {
+            w = 'boulders_7',
+        },
+    },
+
+	{
+        label = 'Medium 40',
+        dir1 = {
+            w = 'boulders_40',
+        },
+    },
+
+	{
+        label = 'Medium 41',
+        dir1 = {
+            w = 'boulders_41',
+        },
+    },
+
+	{
+        label = 'Medium 42',
+        dir1 = {
+            w = 'boulders_42',
+        },
+    },
+
+	{
+        label = 'Medium 43',
+        dir1 = {
+            w = 'boulders_43',
+        },
+    },
+
+	{
+        label = 'Medium 44',
+        dir1 = {
+            w = 'boulders_44',
+        },
+    },
+
+	{
+        label = 'Medium 45',
+        dir1 = {
+            w = 'boulders_45',
+        },
+    },
+
+	{
+        label = 'Medium 46',
+        dir1 = {
+            w = 'boulders_46',
+        },
+    },
+
+	{
+        label = 'Medium 47',
+        dir1 = {
+            w = 'boulders_47',
+        },
+    },
+
+	{
+        label = 'Medium 56',
+        dir1 = {
+            w = 'boulders_56',
+        },
+    },
+
+	{
+        label = 'Medium 57',
+        dir1 = {
+            w = 'boulders_7',
+        },
+    },
+
+	{
+        label = 'Medium 58',
+        dir1 = {
+            w = 'boulders_7',
+        },
+    },
+
+	{
+        label = 'Medium 59',
+        dir1 = {
+            w = 'boulders_59',
+        },
+    },
+
+	{
+        label = 'Medium 60',
+        dir1 = {
+            w = 'boulders_60',
+        },
+    },
+
+	{
+        label = 'Medium 61',
+        dir1 = {
+            w = 'boulders_61',
+        },
+    },
+
+	{
+        label = 'Medium 62',
+        dir1 = {
+            w = 'boulders_62',
+        },
+    },
+
+	{
+        label = 'Medium 63',
+        dir1 = {
+            w = 'boulders_63',
+        },
+    },
+
+
+    {
+        label = '## Large Boulders ##',
+    },
+  
+
+    {
+        label = 'Large 1',
+        dir1 = {
+		w = 'boulders_16',
+		n = 'boulders_19',
+		e = 'boulders_17',
+		s = 'boulders_18',
+        },
+    },
+
+    {
+        label = 'Large 2',
+        dir1 = {
+		w = 'boulders_20',
+		n = 'boulders_23',
+		e = 'boulders_21',
+		s = 'boulders_22',
+        },
+    },
+
+    {
+        label = 'Large 3',
+        dir1 = {
+		w = 'boulders_24',
+		n = 'boulders_27',
+		e = 'boulders_25',
+		s = 'boulders_26',
+        },
+    },
+
+    {
+        label = 'Large 4',
+        dir1 = {
+		w = 'boulders_28',
+		n = 'boulders_31',
+		e = 'boulders_29',
+		s = 'boulders_30',
+        },
+    },
+
+    {
+        label = 'Large 5',
+        dir1 = {
+		w = 'boulders_32',
+		n = 'boulders_33',
+        },
+    },
+
+    {
+        label = 'Large 6',
+        dir1 = {
+		w = 'boulders_34',
+		e = 'boulders_35',
+        },
+    },
+
+
+}
+	
+LAYERS = {
+	'0_Furniture',
+	'0_Furniture1',
+	'0_Furniture2',
+	'0_Furniture3'
+}
+
+
+function options()
+    local items = {}
+    for i=1,#DATA do items[i] = DATA[i].label end
+    return {
+        { name = 'type', label = 'Type:', type = 'list', items = items },
+    }
+end
+
+function setOption(name, value)
+    if name == 'type' and #DATA > 0 then
+    CURDATA = DATA[value]
+    end
+end
+
+function activate()
+    self:setCursorType(TileTool.CurbTool)
+    print 'activate'
+end
+
+function deactivate()
+    print('deactivate')
+end
+
+function mouseMoved(buttons, x, y, modifiers)
+	self:clearToolTiles()	
+	if x < 0 or y < 0 or x >= map:width() or y >= map:height() then
+		self.ok = false
+		return
+	end	
+		if modifiers.alt then
+			local layerName = pickEmptyLayer4(x, y)
+			if layerName then
+				self.ok = true
+				if CURDATA.dir4.w ~= nil then
+					local tile = map:tile(CURDATA.dir4.w)
+					if tile then
+						self:setToolTile(layerName, x, y, tile)
+					end
+				end
+				if CURDATA.dir4.n ~= nil then
+					local tile2 = map:tile(CURDATA.dir4.n)
+					if tile2 then
+						self:setToolTile(layerName, x, y-1, tile2)
+					end
+				end
+				if CURDATA.dir4.e ~= nil then
+					local tile3 = map:tile(CURDATA.dir4.e)
+					if tile3 then
+						self:setToolTile(layerName, x+1, y, tile3)        
+					end
+				end
+				if CURDATA.dir4.s ~= nil then
+					local tile4 = map:tile(CURDATA.dir4.s)
+					if tile4 then
+						self:setToolTile(layerName, x+1, y-1, tile4)
+					end
+				end
+			end
+		elseif modifiers.control then
+    local layerName = pickEmptyLayer3(x, y)
+    if layerName then
+        self.ok = true
+        if CURDATA.dir3.w ~= nil then
+            local tile = map:tile(CURDATA.dir3.w)
+            if tile then
+                self:setToolTile(layerName, x, y, tile)
+            end
+        end
+        if CURDATA.dir3.n ~= nil then
+            local tile2 = map:tile(CURDATA.dir3.n)
+            if tile2 then
+                self:setToolTile(layerName, x, y-1, tile2)
+            end
+        end
+        if CURDATA.dir3.e ~= nil then
+            local tile3 = map:tile(CURDATA.dir3.e)
+            if tile3 then
+                self:setToolTile(layerName, x+1, y, tile3)        
+            end
+        end
+        if CURDATA.dir3.s ~= nil then
+            local tile4 = map:tile(CURDATA.dir3.s)
+            if tile4 then
+                self:setToolTile(layerName, x+1, y-1, tile4)
+            end
+        end
+    end
+		elseif modifiers.shift then
+    local layerName = pickEmptyLayer2(x, y)
+    if layerName then
+        self.ok = true
+        if CURDATA.dir2.w ~= nil then
+            local tile = map:tile(CURDATA.dir2.w)
+            if tile then
+                self:setToolTile(layerName, x, y, tile)
+            end
+        end
+        if CURDATA.dir2.n ~= nil then
+            local tile2 = map:tile(CURDATA.dir2.n)
+            if tile2 then
+                self:setToolTile(layerName, x, y-1, tile2)
+            end
+        end
+        if CURDATA.dir2.e ~= nil then
+            local tile3 = map:tile(CURDATA.dir2.e)
+            if tile3 then
+                self:setToolTile(layerName, x+1, y, tile3)        
+            end
+        end
+        if CURDATA.dir2.s ~= nil then
+            local tile4 = map:tile(CURDATA.dir2.s)
+            if tile4 then
+                self:setToolTile(layerName, x+1, y-1, tile4)
+            end
+        end
+		end
+		else
+    local layerName = pickEmptyLayer1(x, y)
+    if layerName then
+        self.ok = true
+        if CURDATA.dir1.w ~= nil then
+            local tile = map:tile(CURDATA.dir1.w)
+            if tile then
+                self:setToolTile(layerName, x, y, tile)
+            end
+        end
+        if CURDATA.dir1.n ~= nil then
+            local tile2 = map:tile(CURDATA.dir1.n)
+            if tile2 then
+                self:setToolTile(layerName, x, y-1, tile2)
+            end
+        end
+        if CURDATA.dir1.e ~= nil then
+            local tile3 = map:tile(CURDATA.dir1.e)
+            if tile3 then
+                self:setToolTile(layerName, x+1, y, tile3)        
+            end
+        end
+        if CURDATA.dir1.s ~= nil then
+            local tile4 = map:tile(CURDATA.dir1.s)
+            if tile4 then
+                self:setToolTile(layerName, x+1, y-1, tile4)
+            end
+        end
+    end
+		end
+	self.ok = false
+end
+
+
+function mousePressed(buttons, x, y, modifiers)
+    if buttons.left then
+	self.cancel = false
+	self.x, self.y = x, y
+    end
+    if buttons.right then
+	self.cancel = true
+    end
+end
+
+function mouseReleased(buttons, x, y, modifiers)
+if buttons.left and not self.cancel then
+if modifiers.alt then
+local layerName = pickEmptyLayer4(x, y)
+local tile = map:tile(CURDATA.dir4.w)
+local tile2 = map:tile(CURDATA.dir4.n)
+local tile3 = map:tile(CURDATA.dir4.e)
+local tile4 = map:tile(CURDATA.dir4.s)
+if layerName then
+self.ok = true
+if tile then map:tileLayer(layerName):setTile(self.x, self.y, map:tile(CURDATA.dir4.w)) end
+if tile2 then map:tileLayer(layerName):setTile(self.x, self.y-1, map:tile(CURDATA.dir4.n)) end
+if tile3 then map:tileLayer(layerName):setTile(self.x+1, self.y, map:tile(CURDATA.dir4.e)) end
+if tile4 then map:tileLayer(layerName):setTile(self.x+1, self.y-1, map:tile(CURDATA.dir4.s)) end	
+self:applyChanges('Draw '..CURDATA.label)
+end
+elseif modifiers.control then
+local layerName = pickEmptyLayer3(x, y)
+local tile = map:tile(CURDATA.dir3.w)
+local tile2 = map:tile(CURDATA.dir3.n)
+local tile3 = map:tile(CURDATA.dir3.e)
+local tile4 = map:tile(CURDATA.dir3.s)
+if layerName then
+self.ok = true
+if tile then map:tileLayer(layerName):setTile(self.x, self.y, map:tile(CURDATA.dir3.w)) end
+if tile2 then map:tileLayer(layerName):setTile(self.x, self.y-1, map:tile(CURDATA.dir3.n)) end
+if tile3 then map:tileLayer(layerName):setTile(self.x+1, self.y, map:tile(CURDATA.dir3.e)) end
+if tile4 then map:tileLayer(layerName):setTile(self.x+1, self.y-1, map:tile(CURDATA.dir3.s)) end	
+self:applyChanges('Draw '..CURDATA.label)
+end
+elseif modifiers.shift then
+local layerName = pickEmptyLayer2(x, y)
+local tile = map:tile(CURDATA.dir2.w)
+local tile2 = map:tile(CURDATA.dir2.n)
+local tile3 = map:tile(CURDATA.dir2.e)
+local tile4 = map:tile(CURDATA.dir2.s)
+if layerName then
+self.ok = true
+if tile then map:tileLayer(layerName):setTile(self.x, self.y, map:tile(CURDATA.dir2.w)) end
+if tile2 then map:tileLayer(layerName):setTile(self.x, self.y-1, map:tile(CURDATA.dir2.n)) end
+if tile3 then map:tileLayer(layerName):setTile(self.x+1, self.y, map:tile(CURDATA.dir2.e)) end
+if tile4 then map:tileLayer(layerName):setTile(self.x+1, self.y-1, map:tile(CURDATA.dir2.s)) end	
+self:applyChanges('Draw '..CURDATA.label)
+end
+else
+local layerName = pickEmptyLayer1(x, y)
+local tile = map:tile(CURDATA.dir1.w)
+local tile2 = map:tile(CURDATA.dir1.n)
+local tile3 = map:tile(CURDATA.dir1.e)
+local tile4 = map:tile(CURDATA.dir1.s)
+if layerName then
+self.ok = true
+if tile then map:tileLayer(layerName):setTile(self.x, self.y, map:tile(CURDATA.dir1.w)) end
+if tile2 then map:tileLayer(layerName):setTile(self.x, self.y-1, map:tile(CURDATA.dir1.n)) end
+if tile3 then map:tileLayer(layerName):setTile(self.x+1, self.y, map:tile(CURDATA.dir1.e)) end
+if tile4 then map:tileLayer(layerName):setTile(self.x+1, self.y-1, map:tile(CURDATA.dir1.s)) end	
+self:applyChanges('Draw '..CURDATA.label)
+end
+end
+end
+end
+
+function pickEmptyLayer1(x, y)
+	for _,layerName in ipairs(LAYERS) do
+		local layer = map:tileLayer(layerName)
+		if layer then
+			local tile = layer:tileAt(x, y)
+			if not tile then
+				return layerName
+			end
+		end
+	end
+	return nil
+end
+
+function pickEmptyLayer2(x, y)
+	for _,layerName in ipairs(LAYERS) do
+		local layer = map:tileLayer(layerName)
+		if layer then
+			local tile = layer:tileAt(x, y)
+			if not tile then
+				return layerName
+			end
+		end
+	end
+	return nil
+end
+
+function pickEmptyLayer3(x, y)
+	for _,layerName in ipairs(LAYERS) do
+		local layer = map:tileLayer(layerName)
+		if layer then
+			local tile = layer:tileAt(x, y)
+			if not tile then
+				return layerName
+			end
+		end
+	end
+	return nil
+end
+
+function pickEmptyLayer4(x, y)
+	for _,layerName in ipairs(LAYERS) do
+		local layer = map:tileLayer(layerName)
+		if layer then
+			local tile = layer:tileAt(x, y)
+			if not tile then
+				return layerName
+			end
+		end
+	end
+	return nil
+end
