1abc231e5917dfa20d4c1e12b7c068e5332e1617
Unofficial PZ Mapping Discord <pzmapping@gmail.com>
2025-08-06 12:56:54 +0200

    Add files via upload
    Changelog
    
    Changed: Parkingstall Tool draws now 5 instead of 4 lines
    Added: more Fences tool, secondary Fence tool with 5 more Fences - will add more.

---
+++ lua/tool-morefences-data.lua
@@ -0,0 +1,78 @@
+
+
+fence {
+    label = 'Portable Chainlink',
+	west1 = 'fencing_01_83',
+    west2 = 'fencing_01_82',
+    gate_space_w = nil,
+    gate_door_w = nil,
+    north1 = 'fencing_01_80',
+    north2 = 'fencing_01_81',
+    gate_space_n = nil,
+    gate_door_n = nil,
+    nw = 'fencing_01_84',
+    post = nil
+}
+
+fence {
+    label = 'Portable Barbed Wire Chainlink',
+	west1 = 'fencing_01_91',
+    west2 = 'fencing_01_90',
+    gate_space_w = nil,
+    gate_door_w = nil,
+    north1 = 'fencing_01_88',
+    north2 = 'fencing_01_89',
+    gate_space_n = nil,
+    gate_door_n = nil,
+    nw = 'fencing_01_92',
+    post = nil
+}
+
+
+fence {
+    label = 'Tall Chainlink',
+    west1 = 'fencing_01_59',
+    west2 = 'fencing_01_58',
+    gate_space_w = nil,
+    gate_door_w = 'fixtures_doors_fences_01_65',
+	gate_door_w2 = 'fixtures_doors_fences_01_64',
+    north1 = 'fencing_01_56',
+    north2 = 'fencing_01_57',
+    gate_space_n = nil,
+    gate_door_n = 'fixtures_doors_fences_01_66',
+	gate_door_n2 = 'fixtures_doors_fences_01_67',
+    nw = 'fencing_01_60',
+    post = 'fencing_01_61'
+}
+
+fence {
+    label = 'Short Black Metal',
+    west1 = 'fencing_01_2',
+    west2 = 'fencing_01_2',
+    gate_space_w = nil,
+    gate_door_w = 'fixtures_doors_fences_01_0',
+    north1 = 'fencing_01_1',
+    north2 = 'fencing_01_1',
+    gate_space_n = nil,
+    gate_door_n = 'fixtures_doors_fences_01_1',
+    nw = 'fencing_01_3',
+    post = 'fencing_01_0'
+}
+
+
+fence {
+    label = 'Tall Black Metal',
+    west1 = 'fencing_01_67',
+    west2 = 'fencing_01_66',
+    gate_space_w = nil,
+    gate_door_w = 'fixtures_doors_fences_01_20',
+    north1 = 'fencing_01_64',
+    north2 = 'fencing_01_65',
+    gate_space_n = nil,
+    gate_door_n = 'fixtures_doors_fences_01_21',
+    nw = 'fencing_01_68',
+    post = 'fencing_01_69',
+    se = 'fencing_01_69'
+}
+
+
